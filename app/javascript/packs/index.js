7d9a5e5853680d3e5a905b185fbddd31836dc982
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-01-08 14:08:25 -0500

    first commit

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DeadAndCompanyReview from '../react/src/DeadAndCompanyReview';
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  ReactDOM.render(<DeadAndCompanyReview />, document.getElementById('app'));
+})
---
+++ app/javascript/react/src/DeadAndCompanyReview.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const DeadAndCompanyReview = props => {
+  return(
+  <div>hello world</div>
+  )
+}
+
+export default DeadAndCompanyReview
