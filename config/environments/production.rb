71a9b1a9fa41e3c322f92910e51f9e189b10a65f
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-01-17 12:28:39 -0500

    fix heroku background image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,7 @@
 Rails.application.configure do
-    # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = false
-
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -25,15 +23,14 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = false
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.assets.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-config.assets.digest = true
+
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -63,7 +60,7 @@ config.assets.digest = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "dead-and-company-review_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "dead-and-company-reviewr_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
