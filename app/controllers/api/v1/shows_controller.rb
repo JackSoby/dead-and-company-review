67e6a489aec0b331836fd4212944595d3b43a10b
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-01-26 12:36:20 -0500

    factor logic out of controller into model

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -2,41 +2,7 @@ class Api::V1::ShowsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
     def create
-      response = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=5',
-      :headers => { "accept" => "application/json", "x-api-key" => "#{ENV["SETLIST_API_KEY"]}"})
-      response1 = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=4',
-      :headers => { "accept" => "application/json", "x-api-key" => "#{ENV["SETLIST_API_KEY"]}"})
-      response2 = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=3',
-      :headers => { "accept" => "application/json", "x-api-key" => "#{ENV["SETLIST_API_KEY"]}"})
-
-      @dead = JSON.parse(response.body)
-      @dead1 = JSON.parse(response1.body)
-      @dead2 = JSON.parse(response2.body)
-
-        @shows = Array.new()
-        @dead2["setlists"]['setlist'].each do |show|
-            if show['@eventDate'].include?('2015') == true
-          @shows.push("#{show["venue"]['@name']} " + "#{show['@eventDate']}")
-        else
+      @show = Show.api_2015_fetch
+        render json: @show
       end
-    end
-
-    @dead1["setlists"]['setlist'].each do |show|
-        if show['@eventDate'].include?('2015') == true
-      @shows.push("#{show["venue"]['@name']} " + "#{show['@eventDate']}")
-    else
-  end
-end
-  @dead["setlists"]['setlist'].each do |show|
-          if show['@eventDate'].include?('2015') == true
-        @shows.push("#{show["venue"]['@name']} " + "#{show['@eventDate']}")
-      else
-    end
-  end
-
-
-
-
-   render json: @shows
-  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,41 @@ class Show < ApplicationRecord
 
 belongs_to :tour
 
+def self.api_2015_fetch
+  response = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=5',
+  :headers => { "accept" => "application/json", "x-api-key" => "#{ENV["SETLIST_API_KEY"]}"})
+  response1 = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=4',
+  :headers => { "accept" => "application/json", "x-api-key" => "#{ENV["SETLIST_API_KEY"]}"})
+  response2 = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=3',
+  :headers => { "accept" => "application/json", "x-api-key" => "#{ENV["SETLIST_API_KEY"]}"})
+
+  @dead = JSON.parse(response.body)
+  @dead1 = JSON.parse(response1.body)
+  @dead2 = JSON.parse(response2.body)
+
+    @shows = Array.new()
+    @dead2["setlists"]['setlist'].each do |show|
+        if show['@eventDate'].include?('2015') == true
+      @shows.push("#{show["venue"]['@name']} " + "#{show['@eventDate']}")
+    else
+  end
+end
+
+@dead1["setlists"]['setlist'].each do |show|
+    if show['@eventDate'].include?('2015') == true
+  @shows.push("#{show["venue"]['@name']} " + "#{show['@eventDate']}")
+else
+end
+end
+@dead["setlists"]['setlist'].each do |show|
+      if show['@eventDate'].include?('2015') == true
+    @shows.push("#{show["venue"]['@name']} " + "#{show['@eventDate']}")
+  else
+end
+end
+
+return @shows
+
+end
+
 end
