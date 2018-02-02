e557576a1d4c51410fb76d4c2ad366d52d6bc3a0
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-02-02 13:19:45 -0500

    fetch 2017 shows

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -2,15 +2,12 @@ class Api::V1::ShowsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-
-
     if params["_json"] == ":2015 U.S. Tour"
       @show = Show.api_2015_fetch
-
     elsif params["_json"] == ":2016 U.S. Tour"
         @show = Show.api_2016_fetch
-      else
-        @show = Show.api_2015_fetch
+      else params["_json"] == ":2017 Summer Tour"
+        @show = Show.api_2017_summer_tour_fetch
       end
       render json: @show
     end
